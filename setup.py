7035803df1bf64501f1e554c09e095ddcde70b3d
Juti Noppornpitak <juti@statflo.com>
2017-05-31 11:53:24 -0400

    Bumped up the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name         = 'vireo',
-    version      = '0.7.7',
+    version      = '0.7.8',
     description  = 'A library and framework for event-driven application development',
     license      = 'MIT',
     author       = 'Juti Noppornpitak',
